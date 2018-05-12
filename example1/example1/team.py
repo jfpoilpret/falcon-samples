7d78ade510dcd5fabfcf505035c4a39d3eed6c47
jfpoilpret <jfpoilpret@gmail.com>
2018-05-12 11:49:29 +0200

    Add simple POST with tests. Serialize to JSON storage.

--- example1/example1/team.py
+++ example1/example1/team.py
@@ -1,16 +1,24 @@
+import io
 import json
 import falcon
 
+#FIXME issue with id sequence if we allow delete...
 class Team(object):
-    def on_get(self, req,resp):
-        teams = {
-            'teams': [
-                {
-                    'name': 'France',
-                    'href': '/team/1'
-                }
-            ]
-        }
-        resp.body = json.dumps(teams, ensure_ascii = False)
+    JSON_STORAGE = 'example1/data/teams.json'
 
+    def __init__(self):
+        # Read JSON storage
+        with io.open(Team.JSON_STORAGE, 'r') as f:
+            self._teams = json.load(f)
 
+    def on_get(self, req, resp):
+        resp.body = json.dumps(self._teams, ensure_ascii = False)
+
+    def on_post(self, req, resp):
+        new_team = json.load(req.stream)
+        new_team['id'] = len(self._teams) + 1
+        self._teams.append(new_team)
+        with io.open(Team.JSON_STORAGE, 'w') as f:
+            json.dump(self._teams, f, indent = 4, ensure_ascii = False)
+        resp.body = json.dumps(new_team, ensure_ascii = False)
+        resp.status = falcon.HTTP_CREATED
--- example1/tests/test_app.py
+++ example1/tests/test_app.py
@@ -10,14 +10,20 @@ def client():
     return testing.TestClient(api)
 
 def test_list_teams(client):
-    expected = {
-        'teams': [
-            {
-                'name': 'France',
-                'href': '/team/1'
-            }
-        ]
-    }
+    expected = [
+        {
+            "id": 1,
+            "name": "France"
+        },
+        {
+            "id": 2,
+            "name": "Germany"
+        },
+        {
+            "id": 3,
+            "name": "England"
+        }
+    ]
 
     response = client.simulate_get('/team')
     assert response.status == falcon.HTTP_OK
@@ -25,3 +31,15 @@ def test_list_teams(client):
     actual = json.loads(response.text)
     assert actual == expected
 
+def test_create_team(client):
+    new_team = {
+        'name': 'Spain'
+    }
+    response = client.simulate_post(
+        '/team',
+        body = json.dumps(new_team)
+    )
+
+    assert response.status == falcon.HTTP_CREATED
+    created_team = json.loads(response.text)
+    assert created_team['name'] == 'Spain'
