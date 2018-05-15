195958fad97c3738f4298b9931dfd276c24c0d01
jfpoilpret <jfpoilpret@gmail.com>
2018-05-15 21:20:33 +0200

    Remove useless API on team and update unit tests.

--- example3/example3/team.py
+++ example3/example3/team.py
@@ -3,44 +3,23 @@ from marshmallow import fields, Schema
 
 from .model import DBTeam
 
-#TODO review acceptable methods (no reason to create or delete team, even changing team is probably useless)
 class TeamSchema(Schema):
     id = fields.Integer()
     name = fields.String(required=True)
     group = fields.String()
 
 class Teams(object):
-    get_schema = TeamSchema(many = True)
-    schema = TeamSchema()
+    schema = TeamSchema(many = True)
 
     def on_get(self, req, resp):
         req.context['result'] = self._session.query(DBTeam).all()
-
-    def on_post(self, req, resp):
-        #TODO exception handling? (primary key, other constraints...)
-        team = DBTeam(**req.context['json'])
-        self._session.add(team)
-        self._session.commit()
-        self._session.refresh(team)
-        req.context['result'] = team
-        resp.status = falcon.HTTP_CREATED
 
 class Team(object):
     schema = TeamSchema()
 
     def on_get(self, req, resp, id):
-        team = self._session.query(DBTeam, id = id).one_or_none()
+        team = self._session.query(DBTeam).filter_by(id = id).one_or_none()
         if team:
             req.context['result'] = team
         else:
             resp.status = falcon.HTTP_NOT_FOUND
-
-    def on_delete(self, req, resp, id):
-        team = self._session.query(DBTeam, id = id).one_or_none()
-        if not team:
-            resp.status = falcon.HTTP_NOT_FOUND
-        else:
-            self._session.delete(team)
-            resp.status = falcon.HTTP_NO_CONTENT
-
-    #TODO put, patch
--- example3/tests/test_app.py
+++ example3/tests/test_app.py
@@ -10,42 +10,32 @@ def client():
     return testing.TestClient(api)
 
 def test_list_teams(client):
-    expected = [
-        {
-            "id": 1,
-            "name": "France"
-        },
-        {
-            "id": 2,
-            "name": "Germany"
-        },
-        {
-            "id": 3,
-            "name": "England"
-        }
-    ]
-
     response = client.simulate_get('/team')
     assert response.status == falcon.HTTP_OK
 
     actual = json.loads(response.text)
-    assert actual == expected
-
-def test_create_team(client):
-    new_team = {
-        'name': 'Spain'
+    assert len(actual) == 32
+    expected = {
+        'id': 1,
+        'name': 'Egypt',
+        'group': 'Group A'
     }
-    response = client.simulate_post(
-        '/team',
-        body = json.dumps(new_team)
-    )
+    assert actual[0] == expected
+    expected = {
+        'id': 32,
+        'name': 'Senegal',
+        'group': 'Group H'
+    }
+    assert actual[31] == expected
+    
+def test_get_team(client):
+    response = client.simulate_get('/team/11')
+    assert response.status == falcon.HTTP_OK
 
-    assert response.status == falcon.HTTP_CREATED
-    created_team = json.loads(response.text)
-    assert created_team['name'] == 'Spain'
-
-def test_delete_team(client):
-    response =  client.simulate_delete('/team/9999')
-    assert response.status == falcon.HTTP_NOT_FOUND
-    response =  client.simulate_delete('/team/4')
-    assert response.status == falcon.HTTP_NO_CONTENT
+    actual = json.loads(response.text)
+    expected = {
+        'id': 11,
+        'name': 'France',
+        'group': 'Group C'
+    }
+    assert actual == expected
