7d286ed0fb6cd391ab122dbda4e21412e3f6d52b
jfpoilpret <jfpoilpret@gmail.com>
2018-05-25 23:30:01 +0200

    Fix issue with mixed tabs and spaces

--- example4/example4/utils/falcon_util.py
+++ example4/example4/utils/falcon_util.py
@@ -4,22 +4,21 @@ from falcon import HTTPError
 logger = logging.getLogger(__name__)
 
 def update_item_fields(item, keys, values):
-    # type: (dict, list, dict) -> bool
-    update = False
-    for key in keys:
-        if key in values.keys():
-            setattr(item, key, values[key])
-            update = True
-    return update
+	# type: (dict, list, dict) -> bool
+	update = False
+	for key in keys:
+		if key in values.keys():
+			setattr(item, key, values[key])
+			update = True
+	return update
 
 class ExceptionHandler(object):
-    def __init__(self, status, title):
-        # type: (str, str) -> None
-        self._status = status
-        self._title = title
+	def __init__(self, status, title):
+		# type: (str, str) -> None
+		self._status = status
+		self._title = title
 
-    def __call__(self, ex, req, resp, params):
-        # type: (Exception, falcon.Request, falcon.response, dict) -> None
+	def __call__(self, ex, req, resp, params):
+		# type: (Exception, falcon.Request, falcon.response, dict) -> None
 		logger.warning('Exception occurred on request %s %s', req.method, req.uri, exc_info = ex)
-        raise HTTPError(self._status, self._title, str(ex))
-
+		raise HTTPError(self._status, self._title, str(ex))
--- example4/example4/utils/marshmallow_util.py
+++ example4/example4/utils/marshmallow_util.py
@@ -6,34 +6,34 @@ from marshmallow import Schema, fields, validates_schema, ValidationError
 logger = logging.getLogger(__name__)
 
 class ContextMiddleware(object):
-    def __init__(self):
-        self._context = threading.local()
+	def __init__(self):
+		self._context = threading.local()
 
-    def context(self):
-        return self._context
+	def context(self):
+		return self._context
 
-    def process_request(self, req, resp):
-        # type: (falcon.Request, falcon.Response) -> None
-        self._context.uri = req.uri
-        self._context.prefix = req.prefix
+	def process_request(self, req, resp):
+		# type: (falcon.Request, falcon.Response) -> None
+		self._context.uri = req.uri
+		self._context.prefix = req.prefix
 
 # Create context middleware
 context_middleware = ContextMiddleware()
 
 class URLFor(fields.FormattedString):
-    def __init__(self, uri_template, *args, **kwargs):
-        # type: (str, list, dict) -> None
-        self._context = context_middleware.context()
-        fields.FormattedString.__init__(self, uri_template, *args, **kwargs)
+	def __init__(self, uri_template, *args, **kwargs):
+		# type: (str, list, dict) -> None
+		self._context = context_middleware.context()
+		fields.FormattedString.__init__(self, uri_template, *args, **kwargs)
 
-    def _serialize(self, value, key, obj):
-        # type: (object, str, object) -> None
-        return self._context.prefix + fields.FormattedString._serialize(self, value, key, obj)
+	def _serialize(self, value, key, obj):
+		# type: (object, str, object) -> None
+		return self._context.prefix + fields.FormattedString._serialize(self, value, key, obj)
 
 class StrictSchema(Schema):
-    @validates_schema(pass_original=True)
-    def reject_extra_fields(self, data, original_data):
-        unknown = set(original_data) - set(self.fields)
-        if unknown:
+	@validates_schema(pass_original=True)
+	def reject_extra_fields(self, data, original_data):
+		unknown = set(original_data) - set(self.fields)
+		if unknown:
 			logger.info('Unknown field(s) %s in schema %s', str(unknown), self.__class__.__name__)
-            raise ValidationError('Unknown field', unknown)
+			raise ValidationError('Unknown field', unknown)
