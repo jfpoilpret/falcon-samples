d8822216972d58c73fafa398067b4a198e3a2717
jfpoilpret <jfpoilpret@gmail.com>
2018-05-26 22:31:21 +0200

    Add configuration management

---
+++ example4/example4/configuration.py
@@ -0,0 +1,45 @@
+import logging
+import io
+import os
+from yaml import load
+try:
+	from yaml import CLoader as Loader
+except:
+	from yaml import Loader
+
+class Configuration(object):
+	def __init__(self):
+		target = 'DEV'
+		if 'CONFIG' in os.environ.keys():
+			target = os.environ['CONFIG']
+		with io.open('config.yaml', 'r') as f:
+			all_config = load(f)
+		if target not in all_config.keys():
+			target = 'DEV'
+		self._config = all_config[target]
+
+	@property
+	def db_type(self):
+		return self._value('db_type')
+
+	@property
+	def db_name(self):
+		return self._value('db_name')
+
+	@property
+	def drop_db(self):
+		return self._value('drop_db', False)
+
+	@property
+	def log_level(self):
+		return self._get_level(self._value('log_level', 'INFO'))
+
+	def _value(self, key, default = None):
+		return self._config[key] if key in self._config else default
+	
+	def _get_level(self, level_name):
+		levels = list(filter(lambda l: logging.getLevelName(l) == level_name, range(1, logging.CRITICAL)))
+		if levels:
+			return levels[0]
+		else:
+			return logging.INFO
--- example4/example4/init_app.py
+++ example4/example4/init_app.py
@@ -6,25 +6,32 @@ from .utils.sqlalchemy_util import SqlAlchemy
 from .model import create_db, drop_db
 from .utils.auth import Authenticator
 from .initdb import init_db
+from .configuration import Configuration
 
+# Get configuration
+config = Configuration()
+
+logging.basicConfig(filename = '', level = config.log_level)
 logger = logging.getLogger(__name__)
 
 # Create SQLAlchemy engine
 basedir = os.path.abspath(os.path.dirname(__file__))
-database = 'sqlite:///' + os.path.join(basedir, 'data', 'data.sqlite')
+#database = 'sqlite:///' + os.path.join(basedir, 'data', 'data.sqlite')
+database = '%s:///%s' % (config.db_type, os.path.join(basedir, config.db_name))
 engine = create_engine(database)
 sql_middleware = SqlAlchemy(engine)
 
-# Create DB if not exists
-#TODO later make it configurable (command line args)
-logger.debug('Dropping current DB...')
-drop_db(engine)
-logger.debug('Current DB dropped. Recreating new DB...')
-create_db(engine)
-logger.debug('New DB recreated. Initializing DB content.')
-init_db(sql_middleware.new_session())
-logger.debug('DB content initialized.')
-sql_middleware.delete_session()
+# Create DB if required
+#TODO make it smarter: drop if required (DEV, TEST, TRAIN...), create and init if not exists
+if config.drop_db:
+	logger.debug('Dropping current DB...')
+	drop_db(engine)
+	logger.debug('Current DB dropped. Recreating new DB...')
+	create_db(engine)
+	logger.debug('New DB recreated. Initializing DB content.')
+	init_db(sql_middleware.new_session())
+	logger.debug('DB content initialized.')
+	sql_middleware.delete_session()
 
 # Create basic authenticator for use with all Auth backend
 authenticator = Authenticator(sql_middleware)
