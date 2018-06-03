40c74c034155b418dcfce908b15004dbdf6526f9
jfpoilpret <jfpoilpret@gmail.com>
2018-06-03 16:45:19 +0200

    Put more trace (as log) is test assertion.

--- example4/tests/utils.py
+++ example4/tests/utils.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 import base64
+import logging
 import falcon
 from falcon import testing
 from falcon.testing import helpers
 import json
 import pytest
+
+logger = logging.getLogger(__name__)
 
 def href(path):
     return 'http://' + helpers.DEFAULT_HOST + path
@@ -17,7 +20,7 @@ def json_to_datetime(dt):
 
 def assert_dict(expected, actual):
 	for key, value in expected.items():
-		print("assert_dict() %s %s" % (key, str(value)))
+		logger.debug('assert_dict() %s %s %s', key, str(value), str(actual[key]))
 		if isinstance(value, dict):
 			assert_dict(value, actual[key])
 		else:
